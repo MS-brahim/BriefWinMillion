cb61f852881dea09591e20fdb4d7d789d666a205
BrahiMouissi <57224320+MS-brahim@users.noreply.github.com>
2021-03-17 09:22:24 +0100

    dynamic question

--- com/brief_million/frontend/src/pages/participant/Quiz.jsx
+++ com/brief_million/frontend/src/pages/participant/Quiz.jsx
@@ -3,19 +3,27 @@ import axios from 'axios';
 import io from 'socket.io-client';
 // const socket = io.connect('http://localhost:5000');
 const socket = io();
-// socket.on('server',() =>{
-//     console.log('$ server');
-// });
 
 class Quiz extends Component {  
-    state = {
-        questions:[],
-        // currentQuestion: {},
-        // answer: 0,
-        //false_choice: {}
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            questions:[],
+            currentQuestion:{},
+            currentQuestionIndex:0,
+            nextQuestion:{},
+            score:0
+        }
     }
+     
     componentDidMount(){
+        const{questions, currentQuestion, nextQuestion} = this.state; 
+        this.displayQuestions(questions, currentQuestion, nextQuestion)
+        
+    }
+
+    displayQuestions = (questions , currentQuestion, nextQuestion)=>{
+        let {currentQuestionIndex, score} = this.state;
         axios.get('/question',{
             headers: { "Authorization": localStorage.getItem('Particip_token') },
         })
@@ -24,13 +32,29 @@ class Quiz extends Component {
             this.setState({
                 questions: res.data,
             })
-        })
+            if ((this.state.questions.length>1)) {
+            
+                questions = this.state.questions;
+                currentQuestion = questions[currentQuestionIndex];
+                nextQuestion = questions[currentQuestionIndex + 1];
+                // console.log(score);
+                
+                this.setState({
+                    currentQuestion,
+                    nextQuestion,
+                    score,
+                })
+            }
+        })   
     } 
 
-    
-    handleChoice(pAnswer, questionId){
-        if (pAnswer === 2) {
-            console.log('koko');
+    handleChoice(pAnswer, questionId, points){
+       
+        if (pAnswer === this.state.currentQuestion.answer) {
+            let {score, currentQuestion} = this.state
+            score = this.state.score + currentQuestion.points
+            this.setState({score})
+            // console.log(score);
 
             axios.post('/question_token/post',
                 {
@@ -39,39 +63,57 @@ class Quiz extends Component {
                     "id_participant" : localStorage.getItem('idAuthP')
                 }
             )
-            .then(res =>{
-                console.log(res.data);
+            .then(resQsToken =>{
+                console.log('question token',resQsToken.data);   
+                axios.post('/round/post',
+                {
+                    id_group_member:localStorage.getItem('idGroup'),
+                    id_question: questionId,
+                    id_question_token:resQsToken.data._id,
+                }).then(round=>{
+                    console.log('round',round.data);
+                    
+                    axios.post('/round_score/post',
+                    {
+                        id_round:round.data._id,
+                        score: score,
+                    }).then(roundScore=>{
+                        console.log('score',roundScore.data);
+                    }) 
+                })    
             })
+
+
         }
-        console.log(pAnswer, questionId);
+        this.setState(pState => ({
+            currentQuestionIndex: pState.currentQuestionIndex + 1,
+            currentQuestion: pState.currentQuestion,
+            score: pState.score 
+        }), ()=>{
+            this.displayQuestions(this.state.questions, this.state.currentQuestion, this.state.nextQuestion)
+        })
+        // console.log(pAnswer, questionId);
         // console.log({answer, falseChoice, questionId});
-        console.log(localStorage.getItem('idAuthP'));
-        console.log(localStorage.getItem('idGroup'));
+        // console.log(localStorage.getItem('idAuthP'));
+        // console.log(localStorage.getItem('idGroup'));
+        socket.emit('rounded')
     }
     render() { 
-        const {questions} = this.state;
-        
-        const questionList = questions ? (
-
-            questions.map((item ,index )=> {
-                return (
-                    <div key={item._id} className="mt-5">
-                        <span className="card bg-primary text-white p-3 my-3">{item.question}</span>
-                        <ul className="list-group list-group-flush">
-                            <li onClick={() => this.handleChoice((1))} type="button" className="list-group-item">{item.false_choices.choice1}</li>
-                            <li onClick={() => this.handleChoice((2),item._id, item.points)} type="button" className="list-group-item">{item.answer}</li>
-                            <li onClick={() => this.handleChoice((3))} type="button" className="list-group-item">{item.false_choices.choice2}</li>
-                            <li onClick={() => this.handleChoice((4))} type="button" className="list-group-item">{item.false_choices.choice3}</li>
-                        </ul>
-                    </div>
-                )
-            })
-        ):(
-            <div>No Questions yet</div>
-        )
+        const {questions, currentQuestion, currentQuestionIndex, score} = this.state;
+         console.log(score);
         return (
-            <center className="container">
-                {questionList}
+            <center className="container mt-4">
+                {currentQuestionIndex + 1 +'/'+ questions.length}
+                <span className="float-right">Score: {score}</span>
+                <div key={currentQuestion._id} className="mt-5">
+                    <span className="card bg-primary text-white p-3 my-3">{currentQuestion.question}</span>
+                    <div className="row justify-content-md-center">
+                        <span onClick={() => this.handleChoice((currentQuestion.false_choice1),currentQuestion._id, currentQuestion.points)} type="button" className="col-sm-4 bg-dark text-white rounded p-3 m-3">{currentQuestion.false_choice1}</span>
+                        <span onClick={() => this.handleChoice((currentQuestion.answer),currentQuestion._id, currentQuestion.points)} type="button" className="col-sm-4 bg-dark text-white rounded p-3 m-3">{currentQuestion.answer}</span>
+                        <span onClick={() => this.handleChoice((currentQuestion.false_choice2),currentQuestion._id, currentQuestion.points)} type="button" className="col-sm-4 bg-dark text-white rounded p-3 m-3">{currentQuestion.false_choice2}</span>
+                        <span onClick={() => this.handleChoice((currentQuestion.false_choice3),currentQuestion._id, currentQuestion.points)} type="button" className="col-sm-4 bg-dark text-white rounded p-3 m-3">{currentQuestion.false_choice3}</span>
+                    </div>
+                </div>
             </center>
         );
     }
---
+++ com/brief_million/frontend/src/pages/participant/Score.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+ 
+class Score extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() { 
+        return (
+            <div>
+                Score
+            </div>
+        );
+    }
+}
+ 
+export default Score;
